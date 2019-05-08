ed842d09d755b39135da0a0b3befdeec7c9142f1
captainjoko737 <captainjoko212@gmail.com>
2019-05-08 21:57:52 +0700

    fix history transaksi

--- app/Http/Controllers/api/transaksi/TransaksiController.php
+++ app/Http/Controllers/api/transaksi/TransaksiController.php
@@ -36,50 +36,54 @@ class TransaksiController extends Controller
                         ->where('id_user', request()->id_user)
                         ->orderBy('id', 'DESC')
                         ->get();
-// return $listTransaksi;
-        $resultTransaksi = [];
 
         foreach ($listTransaksi as $key => $value) {
-            $noAntrian = $value->no_antrian;
-
-            // GET VOUCHER
-
-            $voucher = 0;
-            $responseVoucher = Voucher::join('voucher', 'voucher.id', '=', 'transaksi_voucher.id_voucher')
-                ->where('transaksi_voucher.id_user', request()->id_user)
-                ->where('transaksi_voucher.no_antrian', $value->no_antrian)
-                ->first();
-
-            if ($responseVoucher) {
-                $voucher = $responseVoucher->value;
-            }
-
-            $dataTransaksi = [];
-            $totalPrice = 0;
-
-            foreach ($listTransaksi as $keys => $values) {
-
-                // $totalPrice += $values->total * $values->quantity;
-                if ($values->no_antrian == $noAntrian) {
-                    array_push($dataTransaksi, $values);
-                }
-            }
-
-            $data = [
-                'no_antrian' => $noAntrian,
-                'total_price' => $totalPrice,
-                'total_payment' => $totalPrice - $voucher,
-                'voucher'       => $voucher,
-                'list'       => $dataTransaksi
-            ];
-
-            array_push($resultTransaksi, $data);
+            $listTransaksi[$key]['total'] = $value->barang->harga * $value->quantity;
         }
+// return $listTransaksi;
+        // $resultTransaksi = [];
+
+        // foreach ($listTransaksi as $key => $value) {
+        //     $noAntrian = $value->no_antrian;
+
+        //     // GET VOUCHER
+
+        //     $voucher = 0;
+        //     $responseVoucher = Voucher::join('voucher', 'voucher.id', '=', 'transaksi_voucher.id_voucher')
+        //         ->where('transaksi_voucher.id_user', request()->id_user)
+        //         ->where('transaksi_voucher.no_antrian', $value->no_antrian)
+        //         ->first();
+
+        //     if ($responseVoucher) {
+        //         $voucher = $responseVoucher->value;
+        //     }
+
+        //     $dataTransaksi = [];
+        //     $totalPrice = 0;
+
+        //     foreach ($listTransaksi as $keys => $values) {
+
+        //         // $totalPrice += $values->total * $values->quantity;
+        //         if ($values->no_antrian == $noAntrian) {
+        //             array_push($dataTransaksi, $values);
+        //         }
+        //     }
+
+        //     $data = [
+        //         'no_antrian' => $noAntrian,
+        //         'total_price' => $totalPrice,
+        //         'total_payment' => $totalPrice - $voucher,
+        //         'voucher'       => $voucher,
+        //         'list'       => $dataTransaksi
+        //     ];
+
+        //     array_push($resultTransaksi, $data);
+        // }
 
         // return $resultTransaksi;
 
-        if ($resultTransaksi) {
-            return response()->json(['status'=> true, 'message'=> 'Success', 'data' => $resultTransaksi]);
+        if ($listTransaksi) {
+            return response()->json(['status'=> true, 'message'=> 'Success', 'data' => $listTransaksi]);
         }else{
             return response()->json(['status'=> false, 'message'=> 'Something went wrong!', 'data' => []]);
         }
